fa3520b163ec0bf2787f3c0edb1bb6825ecbc1f6
shaimoo <92544909+shaimoo@users.noreply.github.com>
2023-01-13 14:49:38 +0200

    Update Tests.java
    fix bugs

--- Part_1/Tests.java
+++ Part_1/Tests.java
@@ -11,7 +11,7 @@ public class Ex2_1Test {
 
     @Test
     public void testGetNumOfLines() {
-        String[] fileNames = Ex2_1.createTextFiles(5, 100, 20);
+        String[] fileNames = Ex2_1.my_createTextFiles(5, 100);
         int numOfLines = Ex2_1.getNumOfLines(fileNames);
         // Verify that the correct number of lines are returned
         assertEquals(100, numOfLines);
@@ -19,7 +19,7 @@ public class Ex2_1Test {
     
     @Test
     public void testGetNumOfLinesThreads() throws InterruptedException {
-        String[] fileNames = Ex2_1.createTextFiles(5, 100, 20);
+        String[] fileNames = Ex2_1.my_createTextFiles(5, 100);
         int numOfLines = Ex2_1.getNumOfLinesThreads(fileNames);
         // Verify that the correct number of lines are returned
         assertEquals(100, numOfLines);
@@ -27,7 +27,7 @@ public class Ex2_1Test {
     
     @Test
     public void testGetNumOfLinesThreadPool() {
-        String[] fileNames = Ex2_1.createTextFiles(5, 100, 20);
+        String[] fileNames = Ex2_1.my_createTextFiles(5, 100);
         int numOfLines = Ex2_1.getNumOfLinesThreadPool(fileNames);
         // Verify that the correct number of
 	assertEquals(100, numOfLines);

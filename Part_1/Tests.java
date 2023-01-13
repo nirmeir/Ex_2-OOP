5b9665764daa965afd815fe09ff63ef5f354f690
shaimoo <92544909+shaimoo@users.noreply.github.com>
2023-01-13 14:37:48 +0200

    Add files via upload

---
+++ Part_1/Tests.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Ex2_1Test {
+    @Test
+    public void testCreateTextFiles() {
+        String[] fileNames = Ex2_1.createTextFiles(5, 100, 20);
+        assertEquals(5, fileNames.length); // Verify that 5 files were created
+        // Additional asserts to check the contents of the files can also be added here
+    }
+
+    @Test
+    public void testGetNumOfLines() {
+        String[] fileNames = Ex2_1.createTextFiles(5, 100, 20);
+        int numOfLines = Ex2_1.getNumOfLines(fileNames);
+        // Verify that the correct number of lines are returned
+        assertEquals(100, numOfLines);
+    }
+    
+    @Test
+    public void testGetNumOfLinesThreads() throws InterruptedException {
+        String[] fileNames = Ex2_1.createTextFiles(5, 100, 20);
+        int numOfLines = Ex2_1.getNumOfLinesThreads(fileNames);
+        // Verify that the correct number of lines are returned
+        assertEquals(100, numOfLines);
+    }
+    
+    @Test
+    public void testGetNumOfLinesThreadPool() {
+        String[] fileNames = Ex2_1.createTextFiles(5, 100, 20);
+        int numOfLines = Ex2_1.getNumOfLinesThreadPool(fileNames);
+        // Verify that the correct number of
+	assertEquals(100, numOfLines);
+    	}
+}
